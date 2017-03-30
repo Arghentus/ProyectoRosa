e38211b5f10f356ba4f467b65d25596da0d7a172
Juan Jesús Fernández Madrid <juange@joinverse.com>
2017-03-30 13:33:55 +0200

    Formateada MainActivity

--- app/src/main/java/com/example/jairo/proyectorosa/MainActivity.java
+++ app/src/main/java/com/example/jairo/proyectorosa/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.jairo.proyectorosa;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 import butterknife.BindView;
@@ -20,23 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
+
     @OnClick(R.id.frasesButton)
-    public void frasesButtonClick(){
-       Intent FrasesActivityIntent = new Intent(MainActivity.this,FrasesActivity.class);
+    public void frasesButtonClick() {
+        Intent FrasesActivityIntent = new Intent(MainActivity.this, FrasesActivity.class);
         startActivity(FrasesActivityIntent);
-
     }
+
     @OnClick(R.id.musicaButton)
-    public void musicaButtonClick(){
-        Intent MusicaActivityIntent = new Intent(MainActivity.this,MusicaActivity.class);
+    public void musicaButtonClick() {
+        Intent MusicaActivityIntent = new Intent(MainActivity.this, MusicaActivity.class);
         startActivity(MusicaActivityIntent);
-
     }
+
     @OnClick(R.id.recuerdosButton)
-    public void GaleriaButtonClick(){
-        Intent GaleriaActivityIntent = new Intent(MainActivity.this,GaleriaActivity.class);
+    public void GaleriaButtonClick() {
+        Intent GaleriaActivityIntent = new Intent(MainActivity.this, GaleriaActivity.class);
         startActivity(GaleriaActivityIntent);
-
     }
-
 }

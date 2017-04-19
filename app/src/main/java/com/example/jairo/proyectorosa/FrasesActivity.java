111280a324964689cb2fb7ba2b41a77bf538b0f4
Juan Jesús Fernández Madrid <juange@joinverse.com>
2017-04-19 10:22:29 +0200

    Aplicado formato a la clase, extraído la inicialización de las vistas. Next step: usar butterknife

--- app/src/main/java/com/example/jairo/proyectorosa/FrasesActivity.java
+++ app/src/main/java/com/example/jairo/proyectorosa/FrasesActivity.java
@@ -2,8 +2,8 @@ package com.example.jairo.proyectorosa;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,14 +13,12 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-
 
 public class FrasesActivity extends AppCompatActivity {
     public static final int READ_BLOCK_SIZE = 200;
@@ -30,51 +28,56 @@ public class FrasesActivity extends AppCompatActivity {
     public Button agregarFraseButton;
     public ListView frasesListView;
     public ArrayAdapter<String> adapter;
-    public ArrayList<String> frasesArrayList;
-    static final String rosa ="Rosa";
-    static final String jairo ="Jairo";
-
-
+    public ArrayList<String> frasesArrayList ;
+    static final String rosa = "Rosa";
+    static final String jairo = "Jairo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frases);
+        initViews();
+        frasesArrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, frasesArrayList);
+        frasesListView.setAdapter(adapter);
+
+        //Eliminar mientras pulsamos
+        frasesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> AdapterView, View view, int i, long l) {
+                final int posicion = i;
+
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(FrasesActivity.this);
+                dialogo1.setTitle("Importante");
+                dialogo1.setMessage("¿Quieres Eliminarla?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        frasesArrayList.remove(posicion);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                    }
+                });
+                dialogo1.show();
+
+                return false;
+            }
+        });
+
+        cargar();
+    }
+
+    private void initViews() {
         frasesEditText = (EditText) findViewById(R.id.fraseEditText);
         rosaRadioButton = (RadioButton) findViewById(R.id.rosaRadioButton);
         jairoRadioButton = (RadioButton) findViewById(R.id.jairoRadioButton);
         agregarFraseButton = (Button) findViewById(R.id.agregarFraseButton);
         frasesListView = (ListView) findViewById(R.id.frasesListView);
-        frasesArrayList = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, frasesArrayList);
-        frasesListView.setAdapter(adapter);
-        //Eliminar mientras pulsamos
-        frasesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-        @Override
-        public boolean onItemLongClick(AdapterView<?> AdapterView, View view, int i, long l) {
-            final int posicion=i;
-
-            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(FrasesActivity.this);
-            dialogo1.setTitle("Importante");
-            dialogo1.setMessage("¿Quieres Eliminarla?");
-            dialogo1.setCancelable(false);
-            dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialogo1, int id) {
-                    frasesArrayList.remove(posicion);
-                    adapter.notifyDataSetChanged();
-                }
-            });
-            dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialogo1, int id) {
-                }
-            });
-            dialogo1.show();
-
-            return false;
-        }
-    });
-        cargar();
     }
+
     //Metodo para Recargar la ListView
     public void cargar() {
         try {
